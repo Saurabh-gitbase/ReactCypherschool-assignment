b7ca9a5e8298a8dfdebebfda3f4dd737ac1ccb5c
Saurabh <49409857+Saurabh-gitbase@users.noreply.github.com>
2020-08-13 09:36:48 +0530

    Update Q2.js

--- Hard/Q2.js
+++ Hard/Q2.js
@@ -1,1 +1,10 @@
-
+var numInStr = (arr) => {
+    let result = [];
+    for(let item of arr)
+        for(let i of item)
+            if(parseInt(i)){
+                result.push(item); 
+		break;
+	    }
+    return result;
+}
