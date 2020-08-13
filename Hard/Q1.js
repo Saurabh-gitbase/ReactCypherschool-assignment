b121c6868f91d95dc8ae1029a1472e174fd220d7
Saurabh <49409857+Saurabh-gitbase@users.noreply.github.com>
2020-08-13 09:37:50 +0530

    Update Q1.js

--- Hard/Q1.js
+++ Hard/Q1.js
@@ -1,1 +1,22 @@
 
+function realType(value) {
+    switch (typeof (value)) {
+        case 'object':
+            if (value instanceof Array)
+                return 'Array';
+            if (value instanceof Date)
+                return 'Date';
+            if (value instanceof RegExp)
+                return 'regexp';
+            if (value instanceof String)
+                return 'String';
+            if (value instanceof Number)
+                return 'Number';
+            else if (value === null)
+                return "null";
+            return 'object';
+            // valueect literals
+        default:
+            return typeof (value);
+    }
+}
