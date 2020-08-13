b788ab89ba151d6f85dfe57cfe7fddaef24c82ec
Saurabh <49409857+Saurabh-gitbase@users.noreply.github.com>
2020-08-13 09:40:07 +0530

    Update Q1.js

--- Medium/Q1.js
+++ Medium/Q1.js
@@ -1,1 +1,13 @@
 
+var revWord = (word) => word.split('').reverse().join('');
+
+var specialReverse = (temp,x) => {
+    temp = temp.split(' '); 
+    temp.forEach((item,index) => {
+    if(item[0] === x){
+        temp[index] = revWord(item);
+        }
+}); 
+temp=temp.join(' ');
+return temp;
+}
