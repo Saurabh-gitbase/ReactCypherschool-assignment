130f178b905bbe0e8d8c7efc250572091b183a25
Saurabh <49409857+Saurabh-gitbase@users.noreply.github.com>
2020-08-13 09:46:00 +0530

    Update Q3.js

--- Medium/Q3.js
+++ Medium/Q3.js
@@ -1,1 +1,18 @@
 
+function removeDuplicate(arr) {
+    if (arr.length === 0 || arr.length === 1) {
+        return arr;
+    }
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(i, 1)
+            }
+        }
+    }
+    return arr;
+}
+
+// One Liner Solutin using Set.
+
+var removeDups = (arr) => [...new Set(arr)] ;
